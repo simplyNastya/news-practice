1a36e5d0803ff13ed01882505e518d1b17a9545f
Anastasiia Koretska <koretska.anastasiia@gmail.com>
2023-02-02 17:59:19 +0200

    Structured project
    Added SASS, JS files

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './js/calendar';
+import './js/category-news-search';
+import './js/input-news-search-fetch';
+import './js/pagination';
+import './js/theme-toggle';
+import './js/user-authorization';
+import './js/weather';
