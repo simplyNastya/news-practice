5b2eb751f8f61ff429faa9e575f945fbb9d8c8ce
simplyNastya <116064159+simplyNastya@users.noreply.github.com>
2023-02-03 11:55:51 +0000

    Deploying to gh-pages from @ simplyNastya/news-practice@707ccb364909944484b21d0a1f92783eca78f504 🚀

--- read.50f8ae69.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".header__search-form"),t=document.querySelector(".header__search-form-input"),r=document.querySelector(".header__search-form-btn");r.addEventListener("click",(function(){e.style.cssText="position: relative;",t.classList.toggle("visible"),r.classList.toggle("btn-transform")}));
-//# sourceMappingURL=read.50f8ae69.js.map
--- read.73282fb7.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".header__search-form"),t=document.querySelector(".header__search-form-input"),r=document.querySelector(".header__search-form-btn");r.addEventListener("click",(function(){e.style.cssText="position: relative;",t.classList.toggle("visible"),r.classList.toggle("btn-transform")}))}();
-//# sourceMappingURL=read.73282fb7.js.map
