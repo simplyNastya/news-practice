e9678948dca6a102645f5e4735fa3f6ebdcb527e
Anastasiia Koretska <koretska.anastasiia@gmail.com>
2023-02-05 21:44:18 +0200

    Refactored code
    Removed hover/focus effects from admin-button in filter-section and give them such effect but throw javaScript

--- src/js/category-news-search.js
+++ src/js/category-news-search.js
@@ -1,9 +1,39 @@
 import axios from "axios"
 
+const categoriesBtnEl = document.querySelector('.dropdown__content-admin-btn')
+const categoriesDropdownContentWrapperEl = document.querySelector('.dropdown__content-wrapper')
+const categoriesArrowDownIconEl = document.querySelector('.dropdown__content-admin-btn-icon-down')
+const categoriesArrowUpIconEl = document.querySelector('.dropdown__content-admin-btn-icon-up')
+
+const othersBtnEl = document.querySelector('.dropdown-tablet__content-admin-btn')
+const othersDropdownContentWrapperEl = document.querySelector('.dropdown-tablet__content-wrapper')
+const othersArrowDownIconEl = document.querySelector('.dropdown-tablet__content-admin-btn-icon-down')
+const othersArrowUpIconEl = document.querySelector('.dropdown-tablet__content-admin-btn-icon-up')
+
 const categoriesDropdownListEl = document.querySelector('.dropdown__content-list')
 const othersDropdownListEl = document.querySelector('.dropdown-tablet__content-list')
 
 const API_KEY = 'B0nM5YVwVGPOQpaqXoXzd3AxL5Kpg75H'
+
+function showCategoriesList() {
+    categoriesDropdownContentWrapperEl.classList.toggle('visible-content')
+    categoriesBtnEl.classList.toggle('change-btn-style')
+    categoriesArrowDownIconEl.classList.toggle('invisible-content')
+    categoriesArrowUpIconEl.classList.toggle('visible-content')
+}
+
+categoriesBtnEl.addEventListener('click', showCategoriesList)
+
+function showOthersList() {
+    othersDropdownContentWrapperEl.classList.toggle('visible-content')
+    othersBtnEl.classList.toggle('change-btn-style')
+    othersArrowDownIconEl.classList.toggle('invisible-content')
+    othersArrowUpIconEl.classList.toggle('visible-content')
+}
+
+othersBtnEl.addEventListener('click', showOthersList)
+
+
 
 async function getCetegoryList() {
     const response = await axios.get(`https://api.nytimes.com/svc/news/v3/content/section-list.json?api-key=${API_KEY}`)
