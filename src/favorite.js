d6c907fad44d3fcacd922814349dfb61bbfab352
Anastasiia Koretska <koretska.anastasiia@gmail.com>
2023-03-31 15:16:17 +0200

    Refactored code
    Cleaned code

--- src/favorite.js
+++ src/favorite.js
@@ -2,5 +2,4 @@ import './js/mobile-menu-open';
 import './js/pagination';
 import './js/theme-toggle';
 import './js/user-authorization';
-// import './js/add-and-remove-favourite-to-storage';
 import './js/markup-favorite-page';
