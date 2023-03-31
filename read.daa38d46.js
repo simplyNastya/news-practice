7a79593851d075c546fff51631727bddb4d6071f
simplyNastya <116064159+simplyNastya@users.noreply.github.com>
2023-03-31 18:19:53 +0000

    Deploying to gh-pages from @ simplyNastya/news-practice@47acb5da72f8298cc4a46b79377098aba2042063 🚀

---
+++ read.daa38d46.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".read__dropdown-content-admin-btn"),t=document.querySelector(".read__dropdown-content-wrapper"),n=document.querySelector(".read__btn-icon-down"),o=document.querySelector(".read__btn-icon-up");e.addEventListener("click",(function(){t.classList.toggle("visible-content"),e.classList.toggle("change-btn-style"),n.classList.toggle("invisible-content"),o.classList.toggle("visible-content")}));
+//# sourceMappingURL=read.daa38d46.js.map
