89116a3a25255cb89b8cb6d3736234482186bf11
Anastasiia Koretska <koretska.anastasiia@gmail.com>
2023-03-31 21:54:59 +0200

    Refactored code
    Added styles

--- src/js/read-page-dropdown.js
+++ src/js/read-page-dropdown.js
@@ -6,7 +6,6 @@ const dropdownArrowUpIconEl = document.querySelector('.read__btn-icon-up')
 // Клик на кнопку откривает список
 function showAlreadyReadNewsList() {
     dropdownContentWrapperEl.classList.toggle('visible-content')
-    dateBtnEl.classList.toggle('change-btn-style')
     dropdownrrowDownIconEl.classList.toggle('invisible-content')
     dropdownArrowUpIconEl.classList.toggle('visible-content')
 }
