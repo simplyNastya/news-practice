86098ef4b46166d70091de3ba2b4af2d15f0aea8
Anastasiia Koretska <koretska.anastasiia@gmail.com>
2023-04-01 23:25:40 +0200

    Update markup-read-page.js
    refactored code

--- src/js/markup-read-page.js
+++ src/js/markup-read-page.js
@@ -88,20 +88,20 @@ function createMarkupAlreadyReadNews() {
   adminBtn.insertAdjacentHTML('beforeend', markupDates);
   alreadyReadListEl.insertAdjacentHTML('beforeend', markupNews);
 
-  // alreadyReadArray.map(item => {
-  //   const favoriteNews = localStorageAPI.load("favorite-news") || []
-  //   favoriteNews.forEach(news => {
-  //     if (news.uri === item.uri) {
-  //       const newsItem = document.getElementById(news.uri)
-  //       if (newsItem) {
-  //         const favoriteBtn = newsItem.querySelector('.news__btn')
-  //         favoriteBtn.classList.add('fav')
-  //         favoriteBtn.textContent = 'Remove from favorite'
-  //         favoriteBtn.style.width = '168px'
-  //   }
-  //   }
-  // })
-  // })
+  alreadyReadArray.map(item => {
+    const favoriteNews = localStorageAPI.load("favorite-news") || []
+    favoriteNews.forEach(news => {
+      if (news.uri === item.uri) {
+        const newsItem = document.getElementById(news.uri)
+        if (newsItem) {
+          const favoriteBtn = newsItem.querySelector('.read__dropdown-content-btn')
+          favoriteBtn.classList.add('fav')
+          favoriteBtn.textContent = 'Remove from favorite'
+          favoriteBtn.style.width = '168px'
+    }
+    }
+  })
+  })
 }
 
 adminBtn.addEventListener('click', createMarkupAlreadyReadNews)
