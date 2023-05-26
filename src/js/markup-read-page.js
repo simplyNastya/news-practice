faf31b87e4639efa75bce743901c330b5be087e7
Anastasiia Koretska <koretska.anastasiia@gmail.com>
2023-05-26 12:55:40 +0200

    Update markup-read-page.js

--- src/js/markup-read-page.js
+++ src/js/markup-read-page.js
@@ -19,14 +19,15 @@ function createMarkupAlreadyReadNews() {
     if (!dates.includes(dateOfRead)) {
       dates.push(dateOfRead);
     }
-    return dates;
-  }, []);
+    return dates
+  }, [])
 
   const markupWrapper = uniqueDates.map((date) => {
     return `<div class="read__dropdown-wrapper">
             <button
               type="button"
               class="read__dropdown-content-admin-btn"
+              data-date="${date}"
             >${date}
             <svg class="read__btn-icon-down">
               <use href="${svg}#icon-arrow-down"></use>
@@ -38,76 +39,150 @@ function createMarkupAlreadyReadNews() {
             <div class="read__dropdown-content-wrapper">
               <ul class="read__dropdown-content-list"></ul>
             </div>
-          </div>`;
-  }).join('');
+          </div>`
+  }).join('')
 
   readSectionContainerEl.insertAdjacentHTML('beforeend', markupWrapper)
 
-  const alreadyReadListEl = document.querySelector('.read__dropdown-content-list')
+  uniqueDates.forEach(date => {
+  const filteredNews = alreadyReadArray.filter(item => item.dateOfRead === date)
+  const markupNewsForDate = filteredNews.map(({ uri, src, alt, title, subtitle, date, href, category, dateOfRead }) => {
+    return `<li
+        class="read__dropdown-content-item"
+        id="${uri}"
+        data-date="${dateOfRead}"
+        >
+         <img
+           src="${src}"
+           alt="${alt}"
+           class="read__dropdown-content-img"
+         />
+         <h2 class="read__dropdown-content-title">
+           ${title}
+         </h2>
+         <h3 class="read__dropdown-content-subtitle">
+           ${subtitle}
+         </h3>
+         <div
+           class="read__dropdown-content-date-link-wrapper"
+         >
+           <p class="read__dropdown-content-date">
+             ${date}
+           </p>
+           <a
+             href="${href}"
+             target="_blank"
+             rel="noopener noreferrer"
+             class="read__dropdown-content-link"
+             >Read more</a
+           >
+         </div>
+         <button
+           type="button"
+           class="read__dropdown-content-btn"
+         >
+           Add to favorite
+         </button>
+         <svg
+             class="read__dropdown-content-btn-icon-add"
+           >
+             <use href="${svg}#icon-heart"></use>
+           </svg>
+           <svg
+             class="read__dropdown-content-btn-icon-remove"
+           >
+             <use href="${svg}#icon-heart"></use>
+           </svg>
+         <p class="read__dropdown-content-category">
+           ${category}
+         </p>
+         <p class="read__dropdown-content-already-read">
+           Already read
+           <svg class="read__dropdown-content-already-read-icon">
+             <use
+               href="${svg}#icon-icon-check"
+             ></use>
+           </svg>
+         </p>
+      </li>`
+  }).join('')
 
-  const markupNews = alreadyReadArray.map(({ uri, src, alt, title, subtitle, date, href, category, dateOfRead }) => {
-            return `<li
-                class="read__dropdown-content-item"
-                id="${uri}"
-                data-date="${dateOfRead}"
-              >
-                <img
-                  src="${src}"
-                  alt="${alt}"
-                  class="read__dropdown-content-img"
-                />
-                <h2 class="read__dropdown-content-title">
-                  ${title}
-                </h2>
-                <h3 class="read__dropdown-content-subtitle">
-                  ${subtitle}
-                </h3>
-                <div
-                  class="read__dropdown-content-date-link-wrapper"
-                >
-                  <p class="read__dropdown-content-date">
-                    ${date}
-                  </p>
-                  <a
-                    href="${href}"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    class="read__dropdown-content-link"
-                    >Read more</a
-                  >
-                </div>
-                <button
-                  type="button"
-                  class="read__dropdown-content-btn"
-                >
-                  Add to favorite
-                </button>
-                <svg
-                    class="read__dropdown-content-btn-icon-add"
-                  >
-                    <use href="${svg}#icon-heart"></use>
-                  </svg>
-                  <svg
-                    class="read__dropdown-content-btn-icon-remove"
-                  >
-                    <use href="${svg}#icon-heart"></use>
-                  </svg>
-                <p class="read__dropdown-content-category">
-                  ${category}
-                </p>
-                <p class="read__dropdown-content-already-read">
-                  Already read
-                  <svg class="read__dropdown-content-already-read-icon">
-                    <use
-                      href="${svg}#icon-icon-check"
-                    ></use>
-                  </svg>
-                </p>
-              </li>`
-        })
-    .join('')
+  const correspondingBtn = document.querySelector(`.read__dropdown-content-admin-btn[data-date="${date}"]`)
+  const contentList = correspondingBtn.nextElementSibling.querySelector('.read__dropdown-content-list')
+  contentList.innerHTML = markupNewsForDate
+})
 
-  alreadyReadListEl.insertAdjacentHTML('beforeend', markupNews);
+  // const markupNews = alreadyReadArray.map(({ uri, src, alt, title, subtitle, date, href, category, dateOfRead }) => {
+  //           return `<li
+  //               class="read__dropdown-content-item"
+  //               id="${uri}"
+  //               data-date="${dateOfRead}"
+  //             >
+  //               <img
+  //                 src="${src}"
+  //                 alt="${alt}"
+  //                 class="read__dropdown-content-img"
+  //               />
+  //               <h2 class="read__dropdown-content-title">
+  //                 ${title}
+  //               </h2>
+  //               <h3 class="read__dropdown-content-subtitle">
+  //                 ${subtitle}
+  //               </h3>
+  //               <div
+  //                 class="read__dropdown-content-date-link-wrapper"
+  //               >
+  //                 <p class="read__dropdown-content-date">
+  //                   ${date}
+  //                 </p>
+  //                 <a
+  //                   href="${href}"
+  //                   target="_blank"
+  //                   rel="noopener noreferrer"
+  //                   class="read__dropdown-content-link"
+  //                   >Read more</a
+  //                 >
+  //               </div>
+  //               <button
+  //                 type="button"
+  //                 class="read__dropdown-content-btn"
+  //               >
+  //                 Add to favorite
+  //               </button>
+  //               <svg
+  //                   class="read__dropdown-content-btn-icon-add"
+  //                 >
+  //                   <use href="${svg}#icon-heart"></use>
+  //                 </svg>
+  //                 <svg
+  //                   class="read__dropdown-content-btn-icon-remove"
+  //                 >
+  //                   <use href="${svg}#icon-heart"></use>
+  //                 </svg>
+  //               <p class="read__dropdown-content-category">
+  //                 ${category}
+  //               </p>
+  //               <p class="read__dropdown-content-already-read">
+  //                 Already read
+  //                 <svg class="read__dropdown-content-already-read-icon">
+  //                   <use
+  //                     href="${svg}#icon-icon-check"
+  //                   ></use>
+  //                 </svg>
+  //               </p>
+  //             </li>`
+  //       })
+  //   .join('')
+
+  // readDropdownContentAdminBtn.forEach(btn => {
+  //   const btnDateAttribute = btn.getAttribute('data-date')
+
+  //   alreadyReadArray.forEach(({ dateOfRead }) => {
+  //     if (btnDateAttribute === dateOfRead) {
+  //       alreadyReadListEl.insertAdjacentHTML('beforeend', markupNews)
+  //     }  
+  //   })
+  // })      
 
   alreadyReadArray.map(item => {
     const favoriteNews = localStorageAPI.load("favorite-news") || []
