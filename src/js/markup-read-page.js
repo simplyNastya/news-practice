4bc45e59ad4bcac8aa46e8a30ea59c3e33783489
Anastasiia Koretska <koretska.anastasiia@gmail.com>
2023-05-26 11:49:36 +0200

    Update list of articles which were read
    Refactored code

--- src/js/markup-read-page.js
+++ src/js/markup-read-page.js
@@ -2,10 +2,6 @@ import localStorageAPI from './storage'
 import imgOps from '../images/desctop/news-main-img.png'
 import svg from '../images/symbol-defs.svg'
 
-const alreadyReadListEl = document.querySelector(".read__dropdown-content-list")
-const adminBtn = document.querySelector('.read__dropdown-content-admin-btn')
-const adminBtnWrapper = document.querySelector('.read__dropdown-wrapper')
-
 const readSectionContainerEl = document.querySelector('.read__section-container')
 
 const alreadyReadArray = localStorageAPI.load("already-read-news") || []
@@ -26,58 +22,77 @@ function createMarkupAlreadyReadNews() {
     return dates;
   }, []);
 
-  const markupDates = uniqueDates.map((date) => {
-    return `<button class="read__dropdown-content-admin-btn ">${date}</button>`;
+  const markupWrapper = uniqueDates.map((date) => {
+    return `<div class="read__dropdown-wrapper">
+            <button
+              type="button"
+              class="read__dropdown-content-admin-btn"
+            >${date}
+            <svg class="read__btn-icon-down">
+              <use href="${svg}#icon-arrow-down"></use>
+            </svg>
+            <svg class="read__btn-icon-up">
+              <use href="${svg}#icon-arrow-up"></use>
+            </svg>
+            </button>
+            <div class="read__dropdown-content-wrapper">
+              <ul class="read__dropdown-content-list"></ul>
+            </div>
+          </div>`;
   }).join('');
+
+  readSectionContainerEl.insertAdjacentHTML('beforeend', markupWrapper)
+
+  const alreadyReadListEl = document.querySelector('.read__dropdown-content-list')
 
   const markupNews = alreadyReadArray.map(({ uri, src, alt, title, subtitle, date, href, category, dateOfRead }) => {
             return `<li
-                class="read__dropdown-content-item "
+                class="read__dropdown-content-item"
                 id="${uri}"
                 data-date="${dateOfRead}"
               >
                 <img
                   src="${src}"
                   alt="${alt}"
-                  class="read__dropdown-content-img "
+                  class="read__dropdown-content-img"
                 />
-                <h2 class="read__dropdown-content-title ">
+                <h2 class="read__dropdown-content-title">
                   ${title}
                 </h2>
-                <h3 class="read__dropdown-content-subtitle ">
+                <h3 class="read__dropdown-content-subtitle">
                   ${subtitle}
                 </h3>
                 <div
-                  class="read__dropdown-content-date-link-wrapper "
+                  class="read__dropdown-content-date-link-wrapper"
                 >
-                  <p class="read__dropdown-content-date ">
+                  <p class="read__dropdown-content-date">
                     ${date}
                   </p>
                   <a
                     href="${href}"
                     target="_blank"
                     rel="noopener noreferrer"
-                    class="read__dropdown-content-link "
+                    class="read__dropdown-content-link"
                     >Read more</a
                   >
                 </div>
                 <button
                   type="button"
-                  class="read__dropdown-content-btn "
+                  class="read__dropdown-content-btn"
                 >
                   Add to favorite
                 </button>
                 <svg
-                    class="read__dropdown-content-btn-icon-add "
+                    class="read__dropdown-content-btn-icon-add"
                   >
                     <use href="${svg}#icon-heart"></use>
                   </svg>
                   <svg
-                    class="read__dropdown-content-btn-icon-remove "
+                    class="read__dropdown-content-btn-icon-remove"
                   >
                     <use href="${svg}#icon-heart"></use>
                   </svg>
-                <p class="read__dropdown-content-category ">
+                <p class="read__dropdown-content-category">
                   ${category}
                 </p>
                 <p class="read__dropdown-content-already-read">
@@ -91,11 +106,7 @@ function createMarkupAlreadyReadNews() {
               </li>`
         })
     .join('')
-  
-  alreadyReadListEl.innerHTML = '';
-  // adminBtnWrapper.innerHTML = markupDates;
 
-  adminBtn.insertAdjacentHTML('beforeend', markupDates);
   alreadyReadListEl.insertAdjacentHTML('beforeend', markupNews);
 
   alreadyReadArray.map(item => {
--- src/read.js
+++ src/read.js
@@ -2,6 +2,6 @@ import './js/mobile-menu-open';
 import './js/pagination';
 import './js/theme-toggle-fav-read';
 import './js/user-authorization';
-import './js/read-page-dropdown';
 import './js/markup-read-page';
+import './js/read-page-dropdown';
 import './js/page-read-add-remove-favorite';
