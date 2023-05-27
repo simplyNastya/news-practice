d237587e8fce1d79d86bbfd0a1534724e57b7506
Anastasiia Koretska <koretska.anastasiia@gmail.com>
2023-05-27 09:23:16 +0200

    Read news rendered to corresponding buttons and open/close dropdown lists
    Refactored code

--- src/js/read-page-dropdown.js
+++ src/js/read-page-dropdown.js
@@ -1,12 +1,19 @@
-const dateBtnEl = document.querySelector('.read__dropdown-content-admin-btn')
-const dropdownContentWrapperEl = document.querySelector('.read__dropdown-content-wrapper')
-const dropdownrrowDownIconEl = document.querySelector('.read__btn-icon-down')
-const dropdownArrowUpIconEl = document.querySelector('.read__btn-icon-up')
+const dateBtnEls = document.querySelectorAll('.read__dropdown-content-admin-btn')
+// const dropdownContentWrapperEl = document.querySelector('.read__dropdown-content-wrapper')
+// const dropdownrrowDownIconEl = document.querySelector('.read__btn-icon-down')
+// const dropdownArrowUpIconEl = document.querySelector('.read__btn-icon-up')
 
 // Клик на кнопку откривает список
-function showAlreadyReadNewsList() {
-    dropdownContentWrapperEl.classList.toggle('visible-content')
-    dropdownrrowDownIconEl.classList.toggle('invisible-content')
-    dropdownArrowUpIconEl.classList.toggle('visible-content')
+function showAlreadyReadNewsList(e) {
+    const currentBtn = e.currentTarget
+    const currentWrapper = currentBtn.nextElementSibling
+    const currentDropdownDownIcon = currentBtn.querySelector('.read__btn-icon-down')
+    const currentDropdownUpIcon = currentBtn.querySelector('.read__btn-icon-up')
+
+    currentWrapper.classList.toggle('visible-content')
+    currentDropdownDownIcon.classList.toggle('invisible-content')
+    currentDropdownUpIcon.classList.toggle('visible-content')
 }
-dateBtnEl.addEventListener('click', showAlreadyReadNewsList)
+dateBtnEls.forEach(dateBtnEl => {
+  dateBtnEl.addEventListener('click', showAlreadyReadNewsList)
+})
